d10ca7a917d0fa418525d325a886db91b191885f
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2003-04-30 15:34:06 +0000

    Rebuilding the tests using AUTOTEST provided by autoconf.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@27 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/tests/dev_ping.c
+++ taco/tests/dev_ping.c
@@ -1,20 +1,20 @@
 
 /*********************************************************************
-
- File:          dev_state.c
-
- Project:       Test Clients for Device Servers
-
- Description:   A simple program for executing dev_ping
-		indefinitely on a single device.
-
-		Usage : dev_ping [device 1]
-
- Author:     	A. Goetz
-
- Original:      July 1997
-
-
+ *
+ * File:        dev_ping.c
+ *
+ * Project:     Test Clients for Device Servers
+ *
+ * Description: A simple program for executing dev_ping
+ *		indefinitely on a single device.
+ *
+ *		Usage : dev_ping [device 1]
+ *
+ * Author:     	A. Goetz
+ *		$Author: jkrueger1 $
+ *
+ * Original:    July 1997
+ *
  *********************************************************************/
 
 #include <Admin.h>
@@ -27,14 +27,19 @@
  * global variables
  */
 
-static char device_name[80];
-static devserver ping_device;
-static long n_total, n_ok, n_notok;
-static long t_min, t_max, t_total;
+char 		device_name[80];
+devserver 	ping_device;
+static long 	n_total = 0, 
+		n_ok = 0, 
+		n_notok = 0;
+static double 	t_min = 1e10, 
+		t_max = 0, 
+		t_total = 0;
 
 void signal_handler(int signal)
 {
-	long t_avg, error;
+	long 	error;
+	double	t_avg = t_total / (double)n_total;; 
 /*
  *
  * ----orion.ctrm.esrf.fr PING Statistics----
@@ -42,87 +47,93 @@ void signal_handler(int signal)
  * round-trip (ms)  min/avg/max = 0/0/1
  *
  */
-
-	t_avg = t_total / n_total;
-
 	printf("\n----%s DEV_PING Statistics----\n",device_name);
-	printf("%d packets transmitted, %d packets successful, %d errors\n",
-                n_total, n_ok, n_notok);
-	printf("round-trip (ms) min/avg/max = %d/%d/%d\n",t_min,t_avg,t_max);
+	printf("%d packets transmitted, %d packets successful, %d errors\n", n_total, n_ok, n_notok);
+	printf("round-trip (ms) min/avg/max = %.3f / %.3f / %.3f\n",t_min,t_avg,t_max);
 
 	dev_free(ping_device,&error);
-
 	exit(0);
 }
 
-main(argc,argv)
-unsigned int argc;
-char **argv;
+int main(int argc, char **argv)
 {
-
-	long readwrite = 0, error;
-	int i, j, n_devices, status;
-	short devstatus;
+	long 	readwrite = 0, 
+		error;
+	int 	i, 
+		j,
+		c, 
+		n_devices, 
+		n_counts = 1000000,
+		status;
+	short 	devstatus;
 	struct timeval t1, t2;
-	long elapsed;
+	double 	elapsed;
 
-	/*debug_flag = (DEBUG_ON_OFF | DBG_TRACE | DBG_API | DBG_SEC);*/
+/*	debug_flag = (DEBUG_ON_OFF | DBG_TRACE | DBG_API | DBG_SEC);*/
 
-
-	switch (argc)	{
-		case 2:
-			strcpy(device_name,argv[1]);
-			status = dev_import(device_name,readwrite,&ping_device,&error);
-			/*printf("dev_import(%s) returned %d\n",device_name,status);*/
-	              	if (status < 0) 
-			{
-				dev_printerror_no(SEND,NULL,error); 
+	while((c = getopt(argc, argv, "c:h")) != -1)
+		switch (c)
+		{
+			case 'c' : 
+				n_counts = atoi(optarg);
+				break;
+			case 'h' :
+			case '?' :
+				fprintf(stderr, "usage: dev_ping [-c num] <device name>\n");
 				exit(1);
-			}
-			n_devices = 1;
-
-			break;
-		default:
-			printf("usage: %s [device name]\n",argv[0]);
-			exit(1);
+		}
+				
+	if (optind != argc -1)	
+	{
+		fprintf(stderr, "usage: dev_ping [-c num] <device name>\n");
+		exit(1);
 	}
+
+	strcpy(device_name,argv[optind]);
+	status = dev_import(device_name,readwrite,&ping_device,&error);
+	/*printf("dev_import(%s) returned %d\n",device_name,status);*/
+	if (status != DS_OK) 
+	{
+		dev_printerror_no(SEND,NULL,error); 
+		exit(1);
+	}
+	n_devices = 1;
+
         (void) signal(SIGINT,  signal_handler);
 	(void) signal(SIGQUIT, signal_handler);
 	(void) signal(SIGTERM, signal_handler);
 
-	n_total = n_ok = n_notok = 0;
-	t_max = 0;
-	t_min = 0;
-	sleep(1);
-	for (j=0;j<1000000;j++)
+	for (j = 0; j < n_counts; j++)
 	{
+		gettimeofday(&t1, NULL);
+		status = dev_ping(ping_device,&error);
+		gettimeofday(&t2, NULL);
+		if (t1.tv_usec > t2.tv_usec)
 		{
-			gettimeofday(&t1, NULL);
-			status = dev_ping(ping_device,&error);
-			gettimeofday(&t2, NULL);
-			elapsed = (t2.tv_sec-t1.tv_sec)*1000;
-			elapsed = (t2.tv_usec-t1.tv_usec)/1000 + elapsed;
-
-			n_total++;
-	              	if (status < 0) 
-			{
-				printf("dev_ping to %s: status=NOTOK sequence=%d time=%d ms\n",device_name,j,elapsed);
-				dev_printerror_no(SEND,NULL,error); 
-				n_notok++;
-			}
-			else
-			{
-				if (t_max == 0) t_max = elapsed;
-				if (elapsed > t_max) t_max = elapsed;
-				if (t_min == 0) t_min = elapsed;
-				if (elapsed < t_min) t_min = elapsed;
-				t_total = t_total + elapsed;
-				printf("dev_ping to %s: status=OK sequence=%d time=%d ms\n",device_name,j,elapsed);
-				n_ok++;
-			}
-			sleep(1);
+			t2.tv_usec += 1000000;
+			t2.tv_sec--;
 		}
+		elapsed = (t2.tv_sec-t1.tv_sec) * 1000;
+		elapsed += (t2.tv_usec-t1.tv_usec) / 1000.;
 
+		n_total++;
+	        if (status < 0) 
+		{
+			printf("dev_ping to %s: status=NOTOK sequence=%d time=%.3f ms\n",device_name, j, elapsed);
+			dev_printerror_no(SEND,NULL,error); 
+			n_notok++;
+		}
+		else
+		{
+			if (elapsed > t_max) 
+				t_max = elapsed;
+			if (elapsed < t_min) 
+				t_min = elapsed;
+			t_total += elapsed;
+			printf("dev_ping to %s: status=OK sequence=%d time=%.3f ms %.3f %.3f %.3f\n",device_name, j, elapsed, t_min, t_max, t_total);
+			n_ok++;
+		}
+		sleep(1);
 	}
-	status = dev_free(ping_device,&error);
+	kill(getpid(), SIGQUIT);
 }
--- taco/tests/fail.res
+++
@@ -1,7 +0,0 @@
-#
-# This file contains only resources for the tests
-#
-
-test/test/devices:	test/device/1 
-
-test/device/1/res1:	"resource 1"
