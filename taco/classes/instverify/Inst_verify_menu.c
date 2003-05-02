e743ba965badf46b7313fc90a98bd98a51866035
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2003-05-02 09:51:57 +0000

    Adopt the output of client and server for the tests.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@32 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/classes/instverify/Inst_verify_menu.c
+++ taco/classes/instverify/Inst_verify_menu.c
@@ -20,239 +20,224 @@
 #include <Admin.h>
 #include <API.h>
 #include <DevServer.h>
-
-long		readwrite = 0;
-char		cmd_string[80];
-devserver	ds;
-long		error;
-int		cmd;
-short		ret;
+/*
+long			readwrite = 0;
+char			cmd_string[80];
+devserver		ds;
+long			error;
+int			cmd;
+short			ret;
 
 DevShort	state;
 DevString	status;
 DevStateFloatReadPoint 	readvalue_argout;
 DevFloat 	setvalue_argin;
+*/
+long ReadCharValue(char lv, char uv,char *par);
+long ReadShortValue(short lv, short uv, short *par);
+long ReadIntValue(int lv, int uv, int *par);
+long ReadFloatValue(float lv, float uv, float *par);
 
-
-main(argc,argv)
-unsigned int 	   argc;
-char      	   **argv;
+int main(int argc, char **argv)
 {
+	long		readwrite = 0;
+	char		cmd_string[80];
+	devserver	ds;
+	long		error;
+	int		cmd;
+	long		ret;
 
-   long		readwrite = 0;
-   char		cmd_string[80];
-   devserver	ds;
-   long		error;
-   int		cmd;
-   long		ret;
-
-   DevShort	state;
-   DevString	status;
-   DevStateFloatReadPoint 	readvalue_argout;
-   DevFloat 	setvalue_argin;
+	DevShort	state;
+	DevString	status;
+	DevStateFloatReadPoint 	readvalue_argout;
+	DevFloat 	setvalue_argin;
 
 
-   if (argc < 2)
-   {
-      printf("usage: %s device-name\n",argv[0]);
-      exit(1);
-   }
+	if (argc < 2)
+	{
+		fprintf(stderr, "usage: %s device-name\n",argv[0]);
+		exit(1);
+	}
 
-   ret = dev_import(argv[1],readwrite,&ds,&error);
-   if(ret != 0)
-   {
-      printf("can't import %s: %s\n",argv[1],dev_error_str(error));
-      exit(1);
-   }
+	if(dev_import(argv[1],readwrite,&ds,&error) != DS_OK)
+	{
+		fprintf(stderr, "can't import %s: %s\n",argv[1],dev_error_str(error));
+		exit(1);
+	}
 
-   while (1)
-   {
-     status = NULL;
-     ret = dev_putget(ds, DevStatus,
-                  NULL, D_VOID_TYPE,
-                  &status, D_STRING_TYPE, &error);
-      printf  ("\n");
-      printf("test_menu for %s (%s)\n",argv[1],ds->device_class);
-      printf("____________________________________\n");
-      printf("1.State \t");
-      printf("2.Status \n");
-   	printf("3.ReadValue  \t");
-   	printf("4.SetValue \n");
-      printf  ("\n");
-      printf("Select one commands (0 to quit) : ? ");
-
+	while (1)
+	{
+		status = 0;
+		ret = dev_putget(ds, DevStatus, NULL, D_VOID_TYPE, &status, D_STRING_TYPE, &error);
+		printf  ("\n");
+		printf("test_menu for %s (%s)\n",argv[1],ds->device_class);
+		printf("____________________________________\n");
+		printf("1.State \t");
+		printf("2.Status\n");
+	   	printf("3.ReadValue  \t");
+	   	printf("4.SetValue\n");
+		printf  ("\n");
+		printf("Select one commands (0 to quit) : ?");
 /*
  * to get around the strange effects of scanf() wait for something read 
  */
-      for( ; gets(cmd_string) == (char *)0 ; );
+		for( ; gets(cmd_string) == (char *)0 ; );
 
-      ret = sscanf(cmd_string,"%d",&cmd);
-      printf  ("\n");
+		ret = sscanf(cmd_string,"%d",&cmd);
+		printf  ("\n");
 
-      switch (cmd) 
-      {
-         case (0) : exit(1);
-         case (1) :
-            ret = dev_putget(ds, DevState,
-                  NULL, D_VOID_TYPE,
-                  &state, D_SHORT_TYPE, &error);
-            if(ret < 0)
-               printf("can't read state: %s\n", dev_error_str(error));
-            else printf("The state is %s (%d)\n",DEVSTATES[state],
-						 state);
-         break;
-
-         case (2) :
-	    status = NULL;
-
-            ret = dev_putget(ds, DevStatus,
-                  NULL, D_VOID_TYPE,
-                  &status, D_STRING_TYPE, &error);
-            if(ret < 0)
-               printf("can't read status: %s\n", dev_error_str(error));
-            else printf("Status :  %s\n", status);
-
-	    if ( dev_xdrfree (D_STRING_TYPE, &status, &error) == DS_NOTOK )
-	       {
-               printf("can't free arguments: %s\n", dev_error_str(error));
-	       }
-
-          break;
-
-         case (3) :
-         /* Insert here your code to read the input argument(s), see the 
-	  * routines below. Exemple:
-	  * ReadIntValue(0, 100, &setvalue);
-	  */
-            ret = dev_putget(ds, DevReadValue,
-                  NULL, D_VOID_TYPE,
-                  &readvalue_argout, D_STATE_FLOAT_READPOINT, &error);
-            if(ret < 0)
-               printf("Can't execute DevReadValue: %s\n", dev_error_str(error)); 
-
-         /* Insert here your code for the command DevReadValue */
-
-	    printf ("Set  value = %3.1f\n", readvalue_argout.set);
-	    printf ("Read value = %3.1f\n", readvalue_argout.read);
-	    printf ("State      = %d\n", readvalue_argout.state);
-
-         break;
-
-         case (4) :
-         /* Insert here your code to read the input argument(s), see the 
-	  * routines below. Exemple:
-	  * ReadIntValue(0, 100, &setvalue);
-	  */
-
-	    printf ("Choose setpoint between 0 and 100 :\n");
-	    ReadFloatValue (0,100,&setvalue_argin);
-
-            ret = dev_putget(ds, DevSetValue,
-                  &setvalue_argin, D_FLOAT_TYPE,
-                  NULL, D_VOID_TYPE, &error);
-            if(ret < 0)
-               printf("Can't execute DevSetValue: %s\n", dev_error_str(error)); 
-
-         break;
-
-         default : break;
-      }
-   }
+		switch (cmd) 
+		{
+			case (0) : 
+				exit(1);
+			case (1) :
+				ret = dev_putget(ds, DevState, NULL, D_VOID_TYPE, &state, D_SHORT_TYPE, &error);
+				if(ret != DS_OK)
+					fprintf(stderr, "can't read state: %s\n", dev_error_str(error));
+				else 
+					printf("The state is %s (%d)\n",DEVSTATES[state], state);
+				break;
+			case (2) :
+				status = NULL;
+				ret = dev_putget(ds, DevStatus, NULL, D_VOID_TYPE, &status, D_STRING_TYPE, &error);
+				if(ret != DS_OK)
+					fprintf(stderr, "can't read status: %s\n", dev_error_str(error));
+				else 
+					printf("Status :  %s\n", status);
+				if ( dev_xdrfree (D_STRING_TYPE, &status, &error) == DS_NOTOK )
+					fprintf(stderr, "can't free arguments: %s\n", dev_error_str(error));
+				break;
+		
+			case (3) :
+/* Insert here your code to read the input argument(s), see the 
+ * routines below. Exemple:
+ * ReadIntValue(0, 100, &setvalue);
+ */
+				ret = dev_putget(ds, DevReadValue, NULL, D_VOID_TYPE, &readvalue_argout, D_STATE_FLOAT_READPOINT, &error);
+				if(ret != DS_OK)
+					fprintf(stderr, "Can't execute DevReadValue: %s\n", dev_error_str(error)); 
+/* Insert here your code for the command DevReadValue */
+				else
+				{
+					printf ("Set  value = %3.1f\n", readvalue_argout.set);
+					printf ("Read value = %3.1f\n", readvalue_argout.read);
+					printf ("State      = %d\n", readvalue_argout.state);
+				}
+				break;
+		case (4) :
+/* Insert here your code to read the input argument(s), see the 
+ * routines below. Exemple:
+ * ReadIntValue(0, 100, &setvalue);
+ */
+				printf ("Choose setpoint between 0 and 100 :\n");
+				ReadFloatValue (0,100,&setvalue_argin);
+				ret = dev_putget(ds, DevSetValue, &setvalue_argin, D_FLOAT_TYPE, NULL, D_VOID_TYPE, &error);
+				if(ret != DS_OK)
+					fprintf(stderr, "Can't execute DevSetValue: %s\n", dev_error_str(error)); 
+				break;
+			default : 
+				break;
+		}
+	}
 }
-/**************************************************************************/
-int ReadIntValue(lv,uv,par)
-/*+************************************************************************
- Purpose:	Reads user input that should be integer
- Returns:	OK/NOK
-*************************************************************************-*/ 
-int lv;                               /* lower limit            */
-int uv;                               /* upper limit            */
-int *par; 				/* Return parameter       */ 
+
+
+/**
+ * Reads user input that should be integer
+ *
+ * @param lv 	the lower limit
+ * @param uv 	the upper limit 
+ * @param par 	the value of the parameter
+ *
+ * @return	DS_OK/DS_NOK
+ */
+long ReadIntValue(int lv, int uv, int *par)
 {
-    char cmd_string[80];
+	char cmd_string[80];
   
-    fgets(cmd_string, 80, stdin);
-    rewind(stdin);
-    if (((cmd_string[0]<'0') || (cmd_string[0]>'9')) && (cmd_string[0] !='-'))
-    	return(DS_NOTOK);
+	fgets(cmd_string, 80, stdin);
+	rewind(stdin);
+	if (((cmd_string[0]<'0') || (cmd_string[0]>'9')) && (cmd_string[0] !='-'))
+		return(DS_NOTOK);
+    
+/* Convert string value into numeric value */
+	sscanf(cmd_string,"%d",par);
+	if ((*par<lv) || (*par>uv)) 
+		return(DS_NOTOK); 
+	return(DS_OK);
+}
+
+/**
+ * Reads user input that should be a lower character 
+ *
+ * @param lv 	the lower limit
+ * @param uv 	the upper limit 
+ * @param par 	the value of the parameter
+ *
+ * @return	DS_OK/DS_NOK
+ */ 
+long ReadCharValue(char lv, char uv,char *par)
+{
+	char cmd_string[80];
+  
+	fgets(cmd_string, 80, stdin);
+	rewind(stdin);
+    
+	if ((cmd_string[0]<'a') || (cmd_string[0]>'z')) 
+		return(DS_NOTOK);
+/* Convert string value into numeric value */
+	*par = cmd_string[0];
+	if ((*par<lv) || (*par>uv)) 
+		return(DS_NOTOK); 
+	return(DS_OK);
+}
+
+/**
+ * Reads user input that should be short
+ *
+ * @param lv 	the lower limit
+ * @param uv 	the upper limit 
+ * @param par 	the value of the parameter
+ *
+ * @return	DS_OK/DS_NOK
+ */ 
+long ReadShortValue(short lv, short uv, short *par)
+{
+	char	cmd_string[80];
+
+	fgets(cmd_string, 80, stdin);
+	rewind(stdin);
+	if (((cmd_string[0]<'0') || (cmd_string[0]>'9')) && (cmd_string[0] !='-') )
+		return(DS_NOTOK);
+/* Convert string value into numeric value */
+	sscanf(cmd_string,"%d", par);
+	if ((*par<lv) || (*par>uv)) 
+		return(DS_NOTOK); 
+	return(DS_OK);
+}
+
+/**
+ * Reads user input that should be a float
+ *
+ * @param lv 	the lower limit
+ * @param uv 	the upper limit 
+ * @param par 	the value of the parameter
+ *
+ * @return	DS_OK/DS_NOK
+ */ 
+long ReadFloatValue(float lv, float uv, float *par)
+{
+	char cmd_string[80];
+  
+	fgets(cmd_string, 80, stdin);
+	rewind(stdin);
+	if (((cmd_string[0]<'0') || (cmd_string[0]>'9')) && (cmd_string[0] !='-'))
+		return(DS_NOTOK);
     
     /* Convert string value into numeric value */
-    sscanf(cmd_string,"%d",par);
-    if ((*par<lv) || (*par>uv)) 
-    	return(DS_NOTOK); 
-    else return(DS_OK);
-}
-
-/**************************************************************************/
-int ReadCharValue(lv,uv,par)
-/*+************************************************************************
- Purpose:	Reads user input that should be short
- Returns:	OK/NOK
-*************************************************************************-*/ 
-char lv;                                        /* lower limit            */
-char uv;                                        /* upper limit            */
-char  *par; 					/* Return parameter       */ 
-{
-    char cmd_string[80];
-    long rr;
-  
-    fgets(cmd_string, 80, stdin);
-    rewind(stdin);
-    
-    if ((cmd_string[0]<'a') || (cmd_string[0]>'z')) 
-    return(DS_NOTOK);
-    /* Convert string value into numeric value */
-    *par = cmd_string[0];
-    if ((*par<lv) || (*par>uv)) 
-    return(DS_NOTOK); 
-    else return(DS_OK);
-}
-
-/**************************************************************************/
-int ReadShortValue(lv,uv,par)
-/*+************************************************************************
- Purpose:	Reads user input that should be short
- Returns:	OK/NOK
-*************************************************************************-*/ 
-short lv;                               /* lower limit            */
-short uv;                               /* upper limit            */
-short *par;				/* Return parameter       */ 
-{
-char cmd_string[80];
-int rr;  
-  fgets(cmd_string, 80, stdin);
-  rewind(stdin);
-  if (((cmd_string[0]<'0') || (cmd_string[0]>'9')) && (cmd_string[0] !='-') )
-     return(DS_NOTOK);
-  /* Convert string value into numeric value */
-  sscanf(cmd_string,"%d",&rr);
-  *par=(short)rr;
-  if ((*par<lv) || (*par>uv)) 
-     return(DS_NOTOK); 
-  else return(DS_OK);
-}
-
-/**************************************************************************/
-int ReadFloatValue(lv,uv,par)
-/*+************************************************************************
- Purpose:	Reads user input that should be a float
- Returns:	OK/NOK
-*************************************************************************-*/ 
-int lv;                               /* lower limit            */
-int uv;                               /* upper limit            */
-float *par; 				/* Return parameter       */ 
-{
-    char cmd_string[80];
-  
-    fgets(cmd_string, 80, stdin);
-    rewind(stdin);
-    if (((cmd_string[0]<'0') || (cmd_string[0]>'9')) && (cmd_string[0] !='-'))
-    	return(DS_NOTOK);
-    
-    /* Convert string value into numeric value */
-    sscanf(cmd_string,"%f",par);
-    if ((*par<lv) || (*par>uv)) 
-    	return(DS_NOTOK); 
-    else return(DS_OK);
+	sscanf(cmd_string,"%f",par);
+	if ((*par<lv) || (*par>uv)) 
+		return(DS_NOTOK); 
+	return(DS_OK);
 }
 
--- taco/classes/instverify/startup.c
+++ taco/classes/instverify/startup.c
@@ -15,6 +15,7 @@
  *		is normally done from startup().
  *
  * Author(s);	J.Meyer 
+ *		$Author: jkrueger1 $
  *
  * Original:	10.5.95	
  *
@@ -37,110 +38,98 @@ extern long debug_flag;
  * pointer to list of devices returned by database.
  *
  */
-static char 			**dev_list;
+static char 		**dev_list;
 static unsigned int	n_devices;
-static Inst_verify		*ds_list;
-/************************************************************************
+static Inst_verify	*ds_list;
 
-Function   :	long startup()
-
-Description:	create, initialise and export all of the objects
-		associated with this server
-
-Arg(s) In  :	svc_name
-
-Arg(s) Out :	error:      error return code
-
-Return(s)  :	minus one on failure, zero otherwise
-
-*************************************************************************/
-
-long startup(svr_name, error)
-char *svr_name;
-long *error;
+/**
+ * create, initialise and export all of the objects associated with this server
+ * 
+ * @param svr_name 	the server name
+ * @param error      	error return code
+ *
+ * @return DS_NOTOK on failure, DS_OK otherwise
+ */
+long startup(char *svr_name, long *error)
 {
-   long		i,	status, n_exported;
-   short		iret;
+	long		i,	
+			status, 
+			n_exported;
+	short		iret;
 
 /* debug_flag = 0xffffffff;*/
 
-   if (db_getdevlist(svr_name,&dev_list,&n_devices,error))
-   {
-      printf("startup(): db_getdevlist() failed: %s\n",dev_error_str(*error));
-      return(-1);
-   }
-   printf("following devices found in static database: \n\n");
-   for (i=0;i<n_devices;i++)
-   {
-      printf("\t%s\n",dev_list[i]);
-   }
+	if (db_getdevlist(svr_name,&dev_list,&n_devices,error))
+	{
+		fprintf(stderr, "startup(): db_getdevlist() failed: %s\n",dev_error_str(*error));
+		return(DS_NOTOK);
+	}
+	printf("following devices found in static database:\n\n");
+	for (i=0;i<n_devices;i++)
+		printf("\t%s\n",dev_list[i]);
 
 /*
  * create, initialise and export all devices served by this server
- ******************************************************************
  */
-   ds_list=(Inst_verify *)malloc(n_devices*sizeof(Inst_verify*));
-   if(ds_list==0)
-   {
-   	printf("can't allocate memory for object structures --> exit\n");
-   	return(DS_NOTOK);
-   }
+	ds_list = (Inst_verify *)malloc(n_devices*sizeof(Inst_verify*));
+	if(ds_list == NULL)
+	{
+		printf("can't allocate memory for object structures --> exit\n");
+		return(DS_NOTOK);
+	}
    
-   for (i=0, n_exported = 0; i < n_devices; i++, n_exported++)
-   {
-      printf("\t\tObject %s is\n",dev_list[i]);
-      fflush(stdout);
+	for (i=0, n_exported = 0; i < n_devices; i++, n_exported++)
+	{
+		printf("\t\tObject %s is\n",dev_list[i]);
+		fflush(stdout);
 
-      if (ds__create(dev_list[i], inst_verifyClass, &(ds_list[i]),error) != 0)
-      {
-   	  printf("create failed: %s\n", dev_error_str(*error));
-   	  continue;
-      }
-      else printf("\t\t- Created\n");
+		if (ds__create(dev_list[i], inst_verifyClass, &(ds_list[i]),error) != DS_OK)
+		{
+			fprintf(stderr, "create failed: %s\n", dev_error_str(*error));
+			continue;
+		}
+		else 
+			printf("\t\t- Created\n");
 
-      /* initialise the newly created inst_verify
-       *******************************************
-       */
-  
-      if(ds__method_finder(ds_list[i],DevMethodInitialise)
-			  (ds_list[i],error)!= 0)
-      {
-      	   printf("initialise failed: %s\n", dev_error_str(*error));
-   	   continue;
-      }
-      else printf("\t\t- Initialised\n");
+/* 
+ * initialise the newly created inst_verify
+ */
+		if(ds__method_finder(ds_list[i],DevMethodInitialise) (ds_list[i],error)!= DS_OK)
+		{
+			fprintf(stderr, "initialise failed: %s\n", dev_error_str(*error));
+			continue;
+		}
+		else 
+			printf("\t\t- Initialised\n");
 
-      /*  now export it to the outside world 
-       *******************************************
-       */
- 
-      if (ds__method_finder(ds_list[i],DevMethodDevExport)
-			   (dev_list[i],ds_list[i],error) != 0)
-      {
-   	   printf("export failed: %s\n", dev_error_str(*error));
-   	   continue;
-      }
-      else printf("\t\t- Exported\n\n");
-   }
+/*  
+ * now export it to the outside world 
+ */
+		if (ds__method_finder(ds_list[i],DevMethodDevExport) (dev_list[i],ds_list[i],error) != DS_OK)
+		{
+			fprintf(stderr, "export failed: %s\n", dev_error_str(*error));
+			continue;
+		}
+		else 
+			printf("\t\t- Exported\n\n");
+	}
 
-   switch(n_exported)
-   {
-      case 0:	
-   	   printf("No devices exported - inst_verify server exiting\n");
-   	   *error = 0;
-   	   iret = -1;
-   	   break;
-
-      case 1:	
-   	   printf("Inst_verify server running with 1 device exported\n");
-   	   *error = 0;
-   	   iret = 0;
-   	   break;
-
-      default:
-   	   printf("Inst_verify server running %d devices exported\n",n_exported);
-   	   *error = 0;
-   	   iret = 0;
-   }
-   return(iret);
+	switch(n_exported)
+	{
+		case 0:	
+			fprintf(stderr, "No devices exported - inst_verify server exiting\n");
+			*error = 0;
+			iret = DS_NOTOK;
+			break;
+		case 1:	
+			printf("Inst_verify server running with 1 device exported\n");
+			*error = 0;
+			iret = DS_OK;
+			break;
+		default:
+			printf("Inst_verify server running %d devices exported\n",n_exported);
+			*error = 0;
+			iret = DS_OK;
+	}
+	return(iret);
 }
