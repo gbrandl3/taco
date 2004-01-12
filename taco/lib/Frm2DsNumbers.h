de34a6fa385162261f744dffe08467375c1577ec
hgilde1 <hgilde1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2004-01-12 16:58:44 +0000

    Frm2DsNumbers.h added, in order to keep the DevLabviewGenericBase ID in a central place
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@202 cd552e4a-acb0-437c-bff1-0b624b0be6ef

---
+++ taco/lib/Frm2DsNumbers.h
@@ -0,0 +1,33 @@
+/*+*******************************************************************
+
+ File:          Frm2DsNumbers.h
+
+ Project:       Device Servers with SUN-RPC
+
+ Description:   Include file with the definitions for all 
+		device server classes of the FRM2 group 
+
+ Author(s):     Hartmut Gilde
+
+ Original:      January 2004
+
+
+ $Revision: 1.1 $
+ $Date: 2004-01-12 16:58:44 $
+
+ $Author: hgilde1 $
+
+ Copyright (c) 1990 by European Synchrotron Radiation Facility,
+		       Grenoble, France
+
+********************************************************************-*/
+
+#ifndef _Frm2DsNumbers_h
+#define _Frm2DsNumbers_h
+
+#include <DserverTeams.h>
+
+/*------- Labview Generic device server---------*/
+#define DevLabViewGenericBase   (FRMTeamNumber + DS_LSHIFT(1, DS_IDENT_SHIFT))
+
+#endif	/* _Frm2DsNumbers_h */
