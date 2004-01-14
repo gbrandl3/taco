a19eadf3fa8bd99ed7c5c3fa73eccc3df45328ee
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2004-01-14 11:03:34 +0000

    Cleanup of code and add a error output.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@215 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/tests/db_connect.cpp
+++ taco/tests/db_connect.cpp
@@ -10,11 +10,7 @@
 /*
  ***************************************************************************
  * Code for db_connect command. 
- *    Command rule : To delete a device and its resources from the database. *
- *                   The -r option is used if the user does not want device  *
- *		    resources to be also deleted			    *
- *                                                                           *
- *    Synopsis : db_devdel [-r] <device name>               		    *
+ * Synopsis : db_connect 
  ***************************************************************************
  */
 int main(int argc, char *argv[])
@@ -26,6 +22,7 @@ int main(int argc, char *argv[])
 	if (db_import(&error) == -1)
 	{
 		std::cerr << "db_devinfo : Impossible to connect to database server" << std::endl;
+		std::cerr << dev_error_str(errno) << std::endl;
 		exit(-1);
 	}
 	return 0;
