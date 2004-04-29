848440a7a12d5fbeb9a44844ae23caa175b6ad1e
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2004-04-29 11:02:31 +0000

    Start of the substitution of db_fillup.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@402 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/system/dbase/tools/db_fillup.cpp
+++ taco/system/dbase/tools/db_fillup.cpp
@@ -25,7 +25,7 @@
 #include "config.h"
 #include <gdbm.h>
 
-/* Function definitions */
+// Function definitions 
 
 static int 	from_file(void);
 static int 	from_res(void);
@@ -40,8 +40,7 @@ static int 	ask_passwd(void);
 static void 	create_db(void);
 static void 	leave(void);
 
-/* Global variables definitions */
-
+// Global variables definitions
 std::vector<GDBM_FILE>		tid;
 int 				line_ptr;
 char 				sec_first = TRUE;
@@ -66,9 +65,11 @@ int 				TblNum = 0;
 *    Synopsis : db_fillup <0/1>                             		    *
 *                                                                           *
 ****************************************************************************/
-int main(int argc,char *argv[])
+int main(int argc,char **argv)
 {
-/* Arguments number test */
+//
+// Arguments number test
+// 
 	if(argc != 2) 
 	{
 		std::cerr << "db_fillup usage : db_fillup <data source>" << std::endl << std::endl;
@@ -77,8 +78,9 @@ int main(int argc,char *argv[])
 		std::cerr << "               1: from previously backuped database" << std::endl;
 		exit(-1);
 	}
-
-/* Choose the right function according to data source parameter */
+//
+// Choose the right function according to data source parameter 
+//
    	switch (int ds = atoi(argv[1])) 
    	{
 		case 0 : return from_res();
@@ -125,9 +127,9 @@ static int from_res(void)
 		*tmp;
 	struct shmid_ds buf;
 	int 	flags;
-
-/* Get base directory name */
-
+//
+// Get base directory name */
+//
 	if ((ptr = getenv("RES_BASE_DIR")) == NULL)
 	{
 		std::cerr << "db_fillup : Can't find environment variable RES_BASE_DIR" << std::endl;
@@ -135,16 +137,16 @@ static int from_res(void)
 	}
 
 	res_dir = base_dir = ptr;
-
-/* Change working directory */
+//
+// Change working directory */
 	if (chdir(res_dir.c_str()))
 	{
 		perror ("db_fillup : Can't cd to resource base directory \n");
 		return (-1);
 	}
-
-/* Find the dbm_database files */        
-
+//
+// Find the dbm_database files */        
+//
 	if ((ptr = (char *)getenv("DBM_DIR")) == NULL)
 	{
 		std::cerr << "db_fillup: Can't find environment variable DBM_DIR" << std::endl;
@@ -154,13 +156,13 @@ static int from_res(void)
 	dbm_dir = ptr;
 	if (dbm_dir[dbm_dir.length() - 1] != '/')
 		dbm_dir += "/";
-
-/* Create the database tables */
+//
+// Create the database tables */
 	create_db();
 
 	flags = GDBM_WRITER; // O_RDWR;
-
-/* Open database tables of the database */
+//
+// Open database tables of the database */
 	int i = 0;
 	for (std::vector<std::string>::iterator it = tblName.begin(); it != tblName.end(); it++)
 	{
@@ -175,20 +177,20 @@ static int from_res(void)
 		tid.push_back(t);
 		i++;
 	}
-
-/* Get the contents of this directory */
+//
+// Get the contents of this directory */
 	FILE	*file;
 	if ((file = popen("ls -R1p", "r")) == NULL)
 	{
 		std::cerr << "db_fillup : Can't get contents of directory" << std::endl;
 		leave();
 	}
-
-/* pipe examination */
+//
+// pipe examination */
 	while(fgets(line, sizeof(line), file) != NULL)
 	{
 		line[strlen(line) - 1] = '\0';		// remove newline
-/* Change directory name if it's a new one */
+// Change directory name if it's a new one */
 		if (strlen(line) == 0)			// emtpy line
 		{
 			fgets(line,sizeof(line),file);
@@ -204,9 +206,9 @@ static int from_res(void)
 #endif /* linux, sun */
 		    	res_dir = temp;
 		}
-
-/* Is it a file ? If yes, build the right file name */
-
+//
+// Is it a file ? If yes, build the right file name */
+//
 		else if ((strchr(line,(int)'/') == NULL) && ((tmp = strstr(line,".res")) != NULL))
 		{
 			inter = tmp;
@@ -220,8 +222,8 @@ static int from_res(void)
 		}
 
 	}
-
-/* Close database */
+//
+// Close database */
 	for (std::vector<GDBM_FILE>::iterator it = tid.begin(); it < tid.end(); ++it)
 		gdbm_close(*it);
 	tid.clear();
@@ -244,8 +246,8 @@ static int read_file(const std::string f_name)
     			line1[256];
 
 	std::cout << "File name : " << f_name << std::endl;
-
-/* Open resource file */
+//
+// Open resource file */
 	if (!fil.is_open()) 
 	{
 		std::cerr << "db_fillup : Couldn't open resource file " << f_name << std::endl;
@@ -254,8 +256,8 @@ static int read_file(const std::string f_name)
 	}
 	line_ptr = 0;
 
-/*   If the file line is a name definition, call dev_name function. 
-     If the file line is a resource value definition, call rs_val function */
+//   If the file line is a name definition, call dev_name function. 
+//If the file line is a resource value definition, call rs_val function */
 	while(!fil.eof()) 
 	{
         	fil.getline(line,sizeof(line)); 
@@ -329,16 +331,16 @@ static int TestLine(char *line,char *line1,int k)
 		iret = 1,
 		i,
 		l1 = 0;
-
-/* Return error in this line is not a definition line */
+//
+// Return error in this line is not a definition line */
 
 	if ((tmp = strchr(line,(int)':')) == NULL) 
 	{	
 		std::cerr << "No delimiter in definition line ':' found" << std::endl;
 		return(-1);
 	}
-
-/* Change all the letters before the : to lower case */
+//
+// Change all the letters before the : to lower case */
 	diff = (u_int)(tmp - line) + 1;
 	i = 0;
 	for (int j = 0; j < diff; j++)
@@ -347,9 +349,9 @@ static int TestLine(char *line,char *line1,int k)
 			line1[i++] = tolower(line[j]);
 	}
 	line1[i] = 0;
-
-/* Is is a device defintion line ? In this case, all the line must be 
-   translated to lower case letter */
+//
+// Is is a device defintion line ? In this case, all the line must be 
+//translated to lower case letter */
 	if (strstr(line1, "device:") != NULL)
 	{
 		for (int j = diff; j < k; j++)
@@ -359,38 +361,38 @@ static int TestLine(char *line,char *line1,int k)
 		}
 		iret = 0;
 	}
-
-/* Now it is a resource definition line */
+//
+// Now it is a resource definition line */
 	else
 	{
-/* If the last character is \ , this is a resource array definition */
+// If the last character is \ , this is a resource array definition */
 		if (line[k - 1] == '\\')
 			iret = 2;
 		for (int j = diff; j < k; j++)
 		{
-/* If the " character is detected, set a flag. If the flag is already set, reset it */
+// If the " character is detected, set a flag. If the flag is already set, reset it */
 			if (line[j] == '"')
 			{
 				i_string = !i_string;
 				continue;
 			}
-/* When the string flag is set, copy character from the original line to the
-   new one without any modifications */
+// When the string flag is set, copy character from the original line to the
+//   new one without any modifications */
 			if (i_string)
 			{
 				line1[i++] = line[j];
 				l1 = 1;
 				continue;
 			}
-/* If the , character is detected not in a string definition, this is a
-   resource array definition */
+// If the , character is detected not in a string definition, this is a
+//   resource array definition */
 			if (line[j] == ',')
 			{
 				line1[i++] = SEP_ELT;
 				iret = 2;
 				continue;
 			}
-/* Remove space and tab */
+// Remove space and tab */
 	    		if (line[j] != ' ' && line[j] != '\t')
 			{
 				line1[i++] = line[j];
@@ -398,13 +400,13 @@ static int TestLine(char *line,char *line1,int k)
 			}
 		}
 	}
-/* If an odd number of " character has been detected, it is an error */
+// If an odd number of " character has been detected, it is an error */
 	if (i_string)
     	{
 		std::cerr << "incomplete string (missing \"?)" << std::endl;
 		iret = -1;
     	}
-/* Leave function */
+// Leave function */
 	line1[i] = 0;
 	if (strlen(line1) == diff && l1 == 0)
     	{
@@ -440,15 +442,15 @@ static void dev_line(char *line1, std::ifstream &file, const std::string f_name,
     			ind;
     	register char 	*ptr1;
 
-/* Make a copy of the device server network name */
+// Make a copy of the device server network name */
 	tmp = strchr(line1,(int)':');
 	diff = (u_int)(tmp - line1) + 1;
 	base = std::string(line1, diff);
 
-/* Copy the first line in the result buffer */
+// Copy the first line in the result buffer */
 	ptr = line1;
 
-/* Following lines examination (discard space or tab at beginning of line) */
+// Following lines examination (discard space or tab at beginning of line) */
 	k = strlen(line1);
 	while(line1[k - 1] == '\\')
 	{
@@ -466,7 +468,7 @@ static void dev_line(char *line1, std::ifstream &file, const std::string f_name,
 		ptr += line1;
 	}
 
-/* Fill up db with first device name */
+// Fill up db with first device name */
 	ind = 1;
 	ptr1 = strtok(const_cast<char *>(ptr.c_str()), "\\");
 	if (dev_name(ptr1,ind))
@@ -475,8 +477,8 @@ static void dev_line(char *line1, std::ifstream &file, const std::string f_name,
 			<< "            at line " << line_ptr << std::endl;
 		leave();
 	} 
-
-/* Fill up db with the other device names */
+//
+// Fill up db with the other device names */
 	while((ptr1 = strtok(NULL,"\\")) != NULL)
 	{
 		ind++;
@@ -518,18 +520,18 @@ static int dev_name(const std::string line, int numb)
 	std::string		lin(line);
 
 	std::cerr << "dev_name = " << lin << std::endl;
-/* Verify device name syntax */
+// Verify device name syntax */
 	if (std::count(lin.begin(), lin.end(), '/') != 4)
 		return(ERR_DEVNAME);
 
-/* Initialize host name, device type and device class */
+// Initialize host name, device type and device class */
 
 	strcpy(dev.h_name,"not_exp");
 	strcpy(dev.d_type,"unknown");
 	strcpy(dev.d_class,"unknown");
 	strcpy(dev.proc_name,"unknown");
 
-/* Get device server class */
+// Get device server class */
 	if ((pos = lin.find_first_of("/")) > sizeof(dev.ds_class) - 1)
 	{
 		std::cerr << "Device server class to long (max. " << (sizeof(dev.ds_class) - 1) << " chars)" << std::endl; 
@@ -539,7 +541,7 @@ static int dev_name(const std::string line, int numb)
 	dev.ds_class[pos] = '\0';
 	lin.erase(0, pos + 1);
 
-/* Get device server name */
+// Get device server name */
 	if ((pos = lin.find_first_of("/")) > sizeof(dev.ds_name) - 1)
 	{
 		std::cerr << "Device server name to long (max. " << (sizeof(dev.ds_name) - 1) << " chars)" << std::endl;
@@ -549,7 +551,7 @@ static int dev_name(const std::string line, int numb)
 	dev.ds_name[pos] = '\0';
 	lin.erase(0, pos + 1);
 
-/* Get device name */
+// Get device name */
 	pos = lin.find_first_of(":");
 	lin.erase(0, pos + 1);
 	if (lin.length() > sizeof(dev.d_name) - 1)
@@ -561,7 +563,7 @@ static int dev_name(const std::string line, int numb)
 		return(ERR_DEVNAME);
 	strcpy(dev.d_name, lin.c_str());
 
-/* Initialize the other columns */
+// Initialize the other columns */
 	dev.pn = 0;
 	dev.vn = 0;
 	dev.indi = numb;
@@ -581,10 +583,10 @@ static int dev_name(const std::string line, int numb)
 		<< "Device server process name : " << dev.proc_name << std::endl;
 #endif
 
-/* Verify that no device with the same name is already registered in the database */
+// Verify that no device with the same name is already registered in the database */
 	key_sto.dptr = (char *)malloc(MAX_KEY); 
 
-/* Go through the table to detect any double instance */
+// Go through the table to detect any double instance */
 	std::vector<GDBM_FILE>::iterator 	t = tid.begin();
 	for (key = gdbm_firstkey(*t); key.dptr != NULL;key = gdbm_nextkey(*t, key))
 	{
@@ -619,7 +621,7 @@ static int dev_name(const std::string line, int numb)
 	}
 
 
-/* Insert tuple in NAMES table */
+// Insert tuple in NAMES table */
 	if ((keyn.dptr = (char *)malloc(line.length())) == NULL)
 	{
 		std::cerr << "Error in memory allocation for the key." << std::endl;
@@ -709,9 +711,9 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
     			temp[80],
     			*tmp,
     			pat[2];
-
-/* Make a copy of the resource array name */
-
+//
+// Make a copy of the resource array name */
+//
 	std::string::size_type pos = line1.find(':') + 1;
 	if (pos > sizeof(base) - 1)
 	{
@@ -722,8 +724,8 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 	strncpy(base, line1.substr(0, pos).c_str(), pos);
 	base[pos] = 0;
 	l_base = pos;
-
-/* Copy the first line in the resulting buffer */
+//
+// Copy the first line in the resulting buffer */
 	int k = line1.length();
 	if (k > SIZE)
 	{
@@ -739,14 +741,14 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 	if (line1[k - 1] == '\\') 
 		ptr[k - 1] = SEP_ELT;
 
-/* Following line examination */
+// Following line examination */
 	while (line1[k - 1] == '\\') 
 	{
 		file.getline(line, siz_line);
 		line_ptr++;
 		line[strlen(line) - 1] = 0;
 
-/* Verify the new line is not a simple resource definition */
+// Verify the new line is not a simple resource definition */
 		if ((ptr2 = strchr(line,(int)':')) != NULL) 
 		{
 			diff = (u_int)(ptr2 - line);
@@ -762,8 +764,8 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 		}
 		k = strlen(line);
 		int j = 0;
-/* Remove space and tab characters except if they are between two ".
-   Replace the , character by 0x02 except if they are between two ". */
+// Remove space and tab characters except if they are between two ".
+//Replace the , character by 0x02 except if they are between two ". */
 		for (int i = 0; i < k; i++) 
 		{
 			if (line[i] == '"') 
@@ -785,7 +787,7 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 				line1[j++] = line[i];
 		}
 
-/* Error if odd number of " characters */
+// Error if odd number of " characters */
 		if (i_string)
 		{
 			std::cerr << "unmatched \"" << std::endl;
@@ -794,14 +796,14 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 		line1[j] = 0;
 		k = line1.length();
 
-/* Add this new line to the result buffer */
+// Add this new line to the result buffer */
 		strcat(ptr,line1.c_str());
 		int l = strlen(ptr);
 		if (line1[k - 1] == '\\') 
 	    		ptr[l - 1] = (ptr[l - 2] == SEP_ELT) ? '\0' : SEP_ELT;		
 
-/* Test to verify that the array (in ascii characters) is not bigger than
-   the allocated memory and realloc memory if needed. */
+// Test to verify that the array (in ascii characters) is not bigger than
+//  the allocated memory and realloc memory if needed. */
     		if (l > ((k1 * SIZE) - LIM)) 
     		{
 			if ((ptr = (char *)realloc((void *)ptr,(size_t)(l + SIZE))) == 0) 
@@ -813,13 +815,13 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 		}
 	}
 
-/* Make two copies of the "ptr" string */
+// Make two copies of the "ptr" string */
 	ptr2 = (char*)malloc(strlen(ptr) + 1);
 	strcpy(ptr2,ptr);
 	ptr3 = (char*)malloc(strlen(ptr) + 1);
 	strcpy(ptr3,ptr);
 
-/* Fill up db with the first array element */
+// Fill up db with the first array element */
 	ind = 1;
 	pat[0] = SEP_ELT;
 	pat[1] = 0;
@@ -831,8 +833,8 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 		leave();
 	}
 
-/* Fill up the db with the other elements of the resource array */
-/* Reinitialize strtok internal pointer */
+// Fill up the db with the other elements of the resource array */
+// Reinitialize strtok internal pointer */
 	ptr1 = strtok(ptr,pat);
 	while ((ptr1 = strtok(NULL,pat)) != NULL) 
 	{
@@ -847,12 +849,12 @@ static int res_line(std::string line1, std::ifstream &file, const std::string f_
 		}
 	}
 
-/* Free memory */
+// Free memory */
 	free(ptr);
 	free(ptr2);
 	free(ptr3);
 
-/* Leave function */
+// Leave function */
 	return(0);
 }
 
