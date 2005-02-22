7be9af18d2ec70b3f8627bc9de3d93cb3b952460
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2005-02-22 13:27:34 +0000

    Add documentation and remove the threaded startup
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@627 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/bindings/python/TacoServer.py
+++ taco/bindings/python/TacoServer.py
@@ -1,8 +1,5 @@
-import thread
 import DEVCMDS
 import Server
-
-from TacoDevice import *
 
 from DEVCMDS   import *
 from DEV_XDR   import *
@@ -10,11 +7,15 @@ from DEVSTATES import *
 from DEVERRORS import *
 
 class TacoServer:
-	"Taco Python super class"
+	"""
+	This class is the Taco Python super class. All classes used in Python as TACO devices
+	should be derived from this class.
+	"""
 	
 #	Common variables for a class
-	cmd_list = { DevState :[D_VOID_TYPE,D_SHORT_TYPE , 'state', 'DevState'],
-		     DevStatus:[D_VOID_TYPE,D_STRING_TYPE,'status','DevStatus']}
+	cmd_list = { DevState :[D_VOID_TYPE,D_SHORT_TYPE , 'DevState', 'DevState'],
+		     DevStatus:[D_VOID_TYPE,D_STRING_TYPE, 'DevStatus','DevStatus']}
+
 	class_name = "PythonClass"
 	
 #	Valriables for an object
@@ -22,28 +23,47 @@ class TacoServer:
 	dev_state   = DEVUNKNOWN
 	dev_status  = "The device is in an unknown state"
 	
-	def __init__ (self, device_name, device_class=0, command_list=0):
+	def __init__ (self, device_name, device_class = None, command_list = None):
+		"""
+		The constructor 
+	
+		@param device_name
+		"""
 		self.dev_name = device_name
-		if device_class != 0:
+		if device_class != None :
 			self.class_name = device_class
-		if command_list != 0:
+		if command_list != None :
 			self.cmd_list = command_list			
 		return
 
-	def state (self):
-		print 'executing python state method'
+	def DevState (self):
+		"""
+		Returns the device status as a number
+
+		This method may be overwritten in subclasses
+		
+		@see status
+		@return device status as a number
+		"""
 		return self.dev_state
 		
-	def status (self):
-		print 'executing python status method'
+	def DevStatus (self):
+		"""
+		Gets the device status as a string
+
+		This method may be overwritten in subclasses
+
+		@see state
+		@return device status as a human readable string
+		"""
 		return self.dev_status
 
-#
-# 	Print the command list of the object 
-#
 	def CommandList (self):
-		print Tab_dev_head
+		"""
+	 	Prints the command list of the object 
+		"""
 		
+		print Tab_dev_head
 		cmd_list_keys = self.cmd_list.keys()
 		for cmd in cmd_list_keys:
 			cmd_list_values = self.cmd_list[cmd]
@@ -62,31 +82,34 @@ class TacoServer:
 	    		else:
 	       			myout = Tab_dev_type_unk
 				
-			# Get the command string from the resource
-			# datbase
-			
+			# Get the command string from the resource datbase
 			cmd_name = Server.cmd_string (cmd)
 			if (cmd_name == None):
 			   	cmd_name = Tab_dev_type_unk
 					     
 	    		print "%s %s %s" % (myin, myout, cmd_name)
 		return None
-#
-# 	Catch all not defined attributes and call cmd_io()
-# 	to check whether the called symbol is a command name
-#
+
   	def __getattr__ (self, cmd_name):
+		"""
+ 		Catches all not defined attributes and call cmd_io()
+ 		to check whether the called symbol is a command name
+		"""
       		self.command_name = cmd_name
-        	print cmd_name
       		return self.cmd_io
-#
-# 	Check in the module DEVCMD for a valid entry a command name.
-# 	If an entry was found, get its integer value.
-# 	Search with the integer value in the command list to get
-# 	the object methode to execute.
-# 	Execute the correct methode for the requested command neme.
-#
+
    	def cmd_io (self, *par, **kw):
+		"""
+ 		Checks in the module DEVCMD for a valid entry a command name.
+ 		If an entry was found, get its integer value.
+ 		Searches with the integer value in the command list to get
+ 		the object methode to execute.
+ 		Execute the correct methode for the requested command name.
+		@param par
+		@param kw
+
+		@return 
+		"""
 		cmd = DEVCMDS.__dict__[self.command_name]
 		cmd_list_values = self.cmd_list[cmd]
 		method_name = cmd_list_values[2]
@@ -97,22 +120,45 @@ class TacoServer:
 #
 # Methods for server class
 #
-	def get_cmd_list (self):
+	def get_cmd_list (self) :
+		"""
+		Returns the informations of the provided commands
+
+		@return informations of the provided commands
+		"""
 		return self.cmd_list
 		
-	def get_dev_name (self):
+	def get_dev_name (self) :
+		"""
+		Returns the name of the device
+		
+		@return name of the device
+		"""
 		return self.dev_name
 		
-	def get_class_name (self):
+	def get_class_name (self) :
+		"""
+		Returns the name of the class
+
+		@return name of the class
+		"""
 		return self.class_name		
 
-#
-# function for server startup
-#
-
-def server_startup (devices, server_name='test', process_name='Python', nodb=0, pn=0):
+def server_startup (devices, server_name = 'test', process_name = 'Python', nodb = 0, pn = 0):
+	"""
+	This is the main function for server startup
+	
+	@param devices tuple of the created devices should be served by the server
+	@param server_name the personal name of the server. It should be different from 
+		others
+	@param process_name the name of the process itself.
+	@param nodb if this parameter is set to 1 the server will work without the TACO database
+	@param pn if this parameter is differen from 0 the server will try to use this number 
+			as RPC program number. If this number is in use please try another. Normally
+			there is no need to create your own number.
+	"""
 	if nodb == 0:
-		thread.start_new_thread (Server.startup, (process_name, server_name, devices))
+		Server.startup(process_name, server_name, devices)
 	else:
-		thread.start_new_thread (Server.startup_nodb, (process_name, server_name, devices, pn))
+		Server.startup_nodb(process_name, server_name, devices, pn)
 
