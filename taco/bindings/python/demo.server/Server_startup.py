ddbe9d0da58db2d8226ab9d16fd8cf30e385ad84
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2005-02-22 12:59:43 +0000

    Clean up the startup procedure
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@621 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/bindings/python/demo.server/Server_startup.py
+++ taco/bindings/python/demo.server/Server_startup.py
@@ -1,6 +1,5 @@
 #
-# Example script how to start a 
-# Taco device server written in Python
+# Example script how to start a TACO device server written in Python
 #
 
 import TacoServer
@@ -10,30 +9,21 @@ def start():
 	#
 	# Create two device objects
 	#
-	x=MyServer.MyServer ('test/python/test1')
-	y=MyServer.MyServer ('test/python/test2')
+	x = MyServer.MyServer('test/python/test1')
+	y = MyServer.MyServer('test/python/test2')
 
 	#
-	# Put the two objects to be exported 
-	# on the network in a tuple
+	# Export to the network and start the device server thread
 	#
-	dev=(x,y)
-
+	# With a device server definition in the resource database as:
+	# process_name/server_name/device:
 	#
-	# Export to the network and start the 
-	# device server thread
-	#
-	# With a device server definition in the resource 
-	# database as:
+	# e.g.:
 	# Python/test/device:	test/python/test1 \
 	#			test/python/test2
 	#	
 	# 
-	TacoServer.server_startup(dev, process_name='Python', server_name='test')
+	TacoServer.server_startup((x, y), process_name = 'Python', server_name = 'test')
 
 if __name__ == "__main__":
 	start()
-	import time
-	time.sleep(1000)
-		
-				
