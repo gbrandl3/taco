12e1d0e9a860c760aecbe62516118907c20cf7fa
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2005-04-11 15:39:36 +0000

    Add the test of the message server
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@701 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/tests/test_server.cpp
+++ taco/tests/test_server.cpp
@@ -10,19 +10,7 @@
 TestDevice::TestDevice(const std::string name, long &error)
 	: Device(const_cast<char *>(name.c_str()), &error)
 {
-	commands_map[DevState] = DeviceCommandMapEntry(DevState, static_cast<DeviceBaseMemberFunction>(&Device::State),
-				D_VOID_TYPE, D_SHORT_TYPE, 0, "State");
-	commands_map[DevStatus] = DeviceCommandMapEntry(DevStatus, static_cast<DeviceBaseMemberFunction>(&Device::Status),
-				D_VOID_TYPE, D_STRING_TYPE, 0, "Status");
-	commands_map[DevReset] = DeviceCommandMapEntry(DevReset, static_cast<DeviceBaseMemberFunction>(&Device::Reset),
-				D_VOID_TYPE, D_VOID_TYPE, 0, "Reset");
-	commands_map[DevOn] = DeviceCommandMapEntry(DevOn, static_cast<DeviceBaseMemberFunction>(&Device::On),
-				D_VOID_TYPE, D_VOID_TYPE, 0, "On");
-	commands_map[DevOff] = DeviceCommandMapEntry(DevOff, static_cast<DeviceBaseMemberFunction>(&Device::Off),
-				D_VOID_TYPE, D_VOID_TYPE, 0, "Off");
-	commands_map[DevClose] = DeviceCommandMapEntry(DevClose, static_cast<DeviceBaseMemberFunction>(&Device::Close),
-				D_VOID_TYPE, D_VOID_TYPE, 0, "Close");
-	commands_map[SendByteArray] = DeviceCommandMapEntry(SendByteArray, static_cast<DeviceBaseMemberFunction>(&TestDevice::tacoSendByteArray),
+	commands_list[SendByteArray] = DeviceCommandListEntry(SendByteArray, static_cast<DeviceMemberFunction>(&TestDevice::tacoSendByteArray),
 				D_VOID_TYPE, D_VAR_CHARARR, 0, "SendByteArray");
 
 	events_list[HeartBeat] = DeviceEventListEntry(HeartBeat, D_LONG_TYPE, "HeartBeat");
@@ -61,7 +49,7 @@ extern "C" long startup(char *serverName, long *error)
 	char            **dev_list;
 	unsigned int    dev_no;
 
-//        debug_flag = (DEBUG_ON_OFF | DBG_TRACE | DBG_DEV_SVR_CLASS | DBG_API);
+        debug_flag = (DEBUG_ON_OFF | DBG_TRACE | DBG_STARTUP);
 
 	if (db_getdevlist(serverName, &dev_list, &dev_no, error) != DS_OK)
 	{
@@ -70,6 +58,7 @@ extern "C" long startup(char *serverName, long *error)
         }
         else
         {
+		dev_printdebug(DBG_TRACE | DBG_STARTUP, "Number of devices: %d\n", dev_no);
 		for (unsigned int i = 0; i < dev_no; ++i)
 		{
 			long		lError;
@@ -81,6 +70,7 @@ extern "C" long startup(char *serverName, long *error)
 		free(dev_list);
 	}
 	*error = 0;
+        debug_flag = 0;
 	return DS_OK;
 }
 
