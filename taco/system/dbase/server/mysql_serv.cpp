2ec9b492fe7d56820e1726f2e274289cea15351f
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2005-07-25 08:41:01 +0000

    Add LGPL header. Order device names by class.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@868 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/system/dbase/server/mysql_serv.cpp
+++ taco/system/dbase/server/mysql_serv.cpp
@@ -1,3 +1,36 @@
+/******************************************************************************
+ * Toolkit for building distributed control systems or any other distributed system.
+ *
+ * Copyright (c) 1990-2005 by European Synchrotron Radiation Facility,
+ *                            Grenoble, France
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * File:
+ *
+ * Description:
+ *
+ * Authors:
+ *		$Author: jkrueger1 $
+ *
+ * Version:	$Revision: 1.13 $
+ *
+ * Date:	$Date: 2005-07-25 08:40:25 $
+ *
+ */
+
 #include <DevErrors.h>
 #include <MySqlServer.h>
 
@@ -386,7 +419,7 @@ int MySQLServer::db_devlist(std::string dev_na, int *dev_num, db_res *back)
 //
 //  
     std::string query;
-    query = "SELECT NAME FROM device WHERE SERVER = '" + ds_class + "/" + ds_name + "'";
+    query = "SELECT NAME, CLASS FROM device WHERE SERVER = '" + ds_class + "/" + ds_name + "' ORDER BY CLASS";
 #ifdef DEBUG
     std::cout << "MySQLServer::db_devlist(): query " << query << std::endl;
 #endif /* DEBUG */
--- taco/system/dbase/server/mysql_tools.cpp
+++ taco/system/dbase/server/mysql_tools.cpp
@@ -1,3 +1,36 @@
+/******************************************************************************
+ * Toolkit for building distributed control systems or any other distributed system.
+ *
+ * Copyright (c) 1990-2005 by European Synchrotron Radiation Facility,
+ *                            Grenoble, France
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * File:
+ *
+ * Description:
+ *
+ * Authors:
+ *		$Author: jkrueger1 $
+ *
+ * Version:	$Revision: 1.13 $
+ *
+ * Date:	$Date: 2005-07-25 08:41:01 $
+ *
+ */
+
 #include <DevErrors.h>
 #include <MySqlServer.h>
 #include <algorithm>
@@ -802,7 +835,7 @@ svcinfo_svc *MySQLServer::svcinfo_1_svc(db_res *recev)
     {
 	query += ("SERVER = '"+ user_ds_name + "/" + user_pers_name + "'");
     }
-    query += (" AND IOR NOT LIKE 'ior:%'");
+    query += (" AND IOR NOT LIKE 'ior:%' ORDER BY CLASS");
     if (mysql_query(mysql_conn, query.c_str()) != 0)
     {
 	std::cerr << mysql_error(mysql_conn) << std::endl;
