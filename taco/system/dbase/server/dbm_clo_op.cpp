6684ddf19f2769d0b79a1b6eaceb25d02c186abe
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2005-07-25 08:19:09 +0000

    Fixing problems with locking on NFS mounted database files
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@867 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/system/dbase/server/dbm_clo_op.cpp
+++ taco/system/dbase/server/dbm_clo_op.cpp
@@ -1,3 +1,36 @@
+/******************************************************************************
+ * Toolkit for building distributed control systems or any other distributed system.
+ *
+ * Copyright (c) 1990-2005 by European Synchrotron Radiation Facility,
+ *                            Grenoble, France
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * File:
+ *
+ * Description:
+ *
+ * Authors:
+ *		$Author: jkrueger1 $
+ *
+ * Version:	$Revision: 1.12 $
+ *
+ * Date:	$Date: 2005-07-25 08:19:09 $
+ *
+ */
+
 #include <API.h>
 
 #include <DevErrors.h>
@@ -67,7 +100,7 @@ DevLong *NdbmServer::db_clodb_1_svc()
 DevLong *NdbmServer::db_reopendb_1_svc()
 {
 	static DevLong 	errcode;
-	int 		flags = O_RDWR;;
+	int 		flags = O_RDWR;
 	char 		*ptr;
 
 #ifdef DEBUG
@@ -100,9 +133,11 @@ DevLong *NdbmServer::db_reopendb_1_svc()
 			mode_t	old_mask = umask(0);
 			GDBM_FILE t = gdbm_open(const_cast<char *>(dbm_file.c_str()), 0, GDBM_WRCREAT, 0666, NULL);
 			umask(old_mask);
-                	if (t == NULL)
+                	if (t == NULL && gdbm_errno == GDBM_CANT_BE_WRITER)
+				t = gdbm_open(const_cast<char *>(dbm_file.c_str()), 0, GDBM_NOLOCK | GDBM_WRCREAT, 0666, NULL);
+			if (t == NULL)
                 	{
-				std::cerr << "dbm_clo_op : Can't create file " << dbm_file << std::endl;
+				std::cerr << "dbm_clo_op (" << gdbm_errno << "): Can't create file : " << dbm_file << std::endl;
 				errcode = DbErr_DatabaseAccess;
 				return(&errcode);
                 	}
@@ -110,9 +145,14 @@ DevLong *NdbmServer::db_reopendb_1_svc()
 		}
 
 		dbgen.tid[i] = gdbm_open(const_cast<char *>(dbm_file.c_str()), 0, flags, 0666, NULL);
+		if (dbgen.tid[i] == NULL || gdbm_errno == GDBM_CANT_BE_WRITER)
+		{
+			flags |= GDBM_NOLOCK; 
+			dbgen.tid[i] = gdbm_open(const_cast<char *>(dbm_file.c_str()), 0, flags, 0666, NULL);
+		}
 		if (dbgen.tid[i] == NULL)
 		{
-			std::cerr <<"dbm_server : Can't open " << dbgen.TblName[i] << " table" << std::endl; 
+			std::cerr <<"dbm_server (" << gbdm_errno << "): Can't open table : " << dbgen.TblName[i] << std::endl; 
 			errcode = DbErr_DatabaseAccess;
 			return(&errcode);
 		}
