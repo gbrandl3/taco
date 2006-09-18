ee772c16567fa0e9b110e51c731281173314e9a5
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2006-09-18 22:00:11 +0000

    Add copyright information.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@1186 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/system/dbase/tools/_count.h
+++ taco/system/dbase/tools/_count.h
@@ -1,6 +1,36 @@
-/*
- * to fix broken std::count() on Solaris CC
+/******************************************************************************
+ * Toolkit for building distributed control systems or any other distributed system.
+ *
+ * Copyright (c) 1990-2005 by European Synchrotron Radiation Facility,
+ *                            Grenoble, France
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * File:
+ *
+ * Description: to fix broken std::count() on Solaris CC
+ *
+ * Author(s):	Andy Goetz
+ *              $Author: jkrueger1 $
+ *
+ * Version:     $Revision: 1.2 $
+ *
+ * Date:        $Date: 2006-09-18 22:00:11 $
+ *
  */
+
 #ifdef _solaris
 namespace _sol {
         template <class Iterator, class T>
