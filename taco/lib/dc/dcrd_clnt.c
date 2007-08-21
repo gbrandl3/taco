81848a24cf62a1af27cf976487f7ea0f08efeb1c
bourtemb <bourtemb@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2007-08-21 07:46:52 +0000

    Ignore the SIGPIPE signal when executing clnt_call() calls to prevent the client to stop if it doesn't catch the SIGPIPE signal.
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@1521 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/lib/dc/dcrd_clnt.c
+++ taco/lib/dc/dcrd_clnt.c
@@ -23,13 +23,13 @@
  * Description:
  *
  * Author(s):   Emmanuel Taurel
- *              $Author: jkrueger1 $
+ *              $Author: bourtemb $
  * 
  * Original:    1993
  *
- * Version:     $Revision: 1.5 $
+ * Version:     $Revision: 1.6 $
  *
- * Date:        $Date: 2006-09-18 22:13:30 $
+ * Date:        $Date: 2007-08-21 07:44:39 $
  *
  *****************************************************************************/
 
@@ -53,13 +53,20 @@
 #define NULL  0
 #endif  /* NULL */
 
+#include <signal.h>
+
 static struct timeval TIMEOUT = { 3, 0 };
 
+static void sigpipe_handler(int signo)
+{
+	return;
+}
 
 xres_clnt *dc_devget_clnt_1(xdevget *argp, CLIENT *clnt, DevArgument argout, DevType argout_type, long *perr)
 {
 	static xres_clnt 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 /* 
  * Clear only place for the error code coming from server, and initialize 
@@ -69,9 +76,11 @@ xres_clnt *dc_devget_clnt_1(xdevget *argp, CLIENT *clnt, DevArgument argout, Dev
 	memset((char *)&res, 0, sizeof(res));
 	res.xargout_type = argout_type;
 	res.xargout = argout;
-
+	
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_DEVGET,(xdrproc_t)xdr_xdevget, (caddr_t)argp,(xdrproc_t)xdr_xres_clnt, (caddr_t)&res, TIMEOUT);
-
+	signal(SIGPIPE,old_sighandler);
+	
 	if (clnt_stat != RPC_SUCCESS)
 	{
 		dev_printdebug(DBG_ERROR,"devget clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -85,6 +94,10 @@ xres_clnt *dc_devget_clnt_1(xdevget *argp, CLIENT *clnt, DevArgument argout, Dev
 			*perr = DcErr_RPCreception;
 		return (NULL);
 	}
+	else
+	{
+		*perr = 0;
+	}
 	return (&res);
 }
 
@@ -92,11 +105,14 @@ xresv_clnt *dc_devgetv_clnt_1(xdevgetv *argp, CLIENT *clnt, xres_clnt *st_ptr, l
 {
 	static xresv_clnt 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 	res.xresa_clnt.xresv_clnt_val = st_ptr;
-
+	
+	old_sighandler = signal(SIGPIPE,SIG_IGN);
 	clnt_stat = clnt_call(clnt, DC_DEVGETV,(xdrproc_t)xdr_xdevgetv, (caddr_t)argp,(xdrproc_t)xdr_xresv_clnt, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS)
 	{
 		dev_printdebug(DBG_ERROR,"devgetv clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -117,11 +133,14 @@ xresm_clnt *dc_devgetm_clnt_1(mpar *argp, CLIENT *clnt, mint *st_ptr, long *perr
 {
 	static xresm_clnt 	res;
 	enum clnt_stat 	clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 	res.x_clnt.x_clnt_val = st_ptr;
-
+	
+	old_sighandler = signal(SIGPIPE,SIG_IGN);
 	clnt_stat = clnt_call(clnt, DC_DEVGETM,(xdrproc_t)xdr_mpar, (caddr_t)argp,(xdrproc_t)xdr_xresm_clnt, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS)
 	{
 		dev_printdebug(DBG_ERROR,"devgetm clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -142,10 +161,13 @@ outpar *dc_devdef_1(imppar *argp, CLIENT *clnt, long *perr)
 {
 	static outpar 	res;
 	enum clnt_stat 	clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
-
+	
+	old_sighandler = signal(SIGPIPE,SIG_IGN);
 	clnt_stat = clnt_call(clnt, DC_DEVDEF,(xdrproc_t)xdr_imppar, (caddr_t)argp,(xdrproc_t)xdr_outpar, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS)
 	{
 		dev_printdebug(DBG_ERROR,"devdef clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -166,12 +188,15 @@ xres_hist_clnt *dc_devgeth_clnt_1(xdevgeth *argp, CLIENT *clnt, xresh_clnt *st_p
 {
 	static xres_hist_clnt	res;
 	enum clnt_stat		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
 	res.xresb_clnt.xresh_clnt_val = st_ptr;
-
+	
+	old_sighandler = signal(SIGPIPE,SIG_IGN);
 	clnt_stat = clnt_call(clnt, DC_DEVGET_H,(xdrproc_t)xdr_xdevgeth, (caddr_t)argp,(xdrproc_t)xdr_xres_hist_clnt, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS)
 	{
 		dev_printdebug(DBG_ERROR,"devget_history clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -191,8 +216,11 @@ xres_hist_clnt *dc_devgeth_clnt_1(xdevgeth *argp, CLIENT *clnt, xresh_clnt *st_p
 int dc_rpc_check_clnt_1(CLIENT *clnt, char **res, long *perr)
 {
 	enum clnt_stat clnt_stat;
-
+	void (* old_sighandler)(int);
+	
+	old_sighandler = signal(SIGPIPE,SIG_IGN);
 	clnt_stat = clnt_call(clnt, RPC_CHECK,(xdrproc_t)xdr_void, NULL,(xdrproc_t)xdr_wrapstring,(caddr_t)res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS)
 	{
 		dev_printdebug(DBG_ERROR,"rpc_check clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
--- taco/lib/dc/dcwr_clnt.c
+++ taco/lib/dc/dcwr_clnt.c
@@ -23,13 +23,13 @@
  * Description:
  * 
  * Author(s):   Emmanuel Taurel
- *		$Author: jkrueger1 $
+ *		$Author: bourtemb $
  *
  * Original:    1992
  *
- * Version:     $Revision: 1.6 $
+ * Version:     $Revision: 1.7 $
  *
- * Date:        $Date: 2006-09-18 22:13:30 $
+ * Date:        $Date: 2007-08-21 07:46:52 $
  *
  ******************************************************************************/
 
@@ -47,16 +47,25 @@
 #define NULL  (0)
 #endif  /* NULL */
 
+#include <signal.h>
+static void sigpipe_handler(int signo)
+{
+	return;
+}
+
 static struct timeval TIMEOUT = { 3, 0 };
 
 dc_xdr_error *dc_open_1(dc_open_in *argp,CLIENT *clnt,long *perr)
 {
 	static dc_xdr_error 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_OPEN,(xdrproc_t)xdr_dc_open_in, (caddr_t)argp,(xdrproc_t)xdr_dc_xdr_error, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"dc_open clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -75,10 +84,13 @@ dc_xdr_error *dc_close_1(name_arr *argp,CLIENT *clnt,long *perr)
 {
 	static dc_xdr_error 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_CLOSE,(xdrproc_t)xdr_name_arr, (caddr_t)argp,(xdrproc_t)xdr_dc_xdr_error, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"dc_close clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -97,10 +109,13 @@ dc_xdr_error *dc_dataput_1(dev_datarr *argp,CLIENT *clnt,long *perr)
 {
 	static dc_xdr_error 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_DATAPUT,(xdrproc_t)xdr_dev_datarr, (caddr_t)argp,(xdrproc_t)xdr_dc_xdr_error, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"dc_dataput clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -119,10 +134,13 @@ dc_infox_back *dc_info_1(void *argp,CLIENT *clnt,long *perr)
 {
 	static dc_infox_back 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_INFO,(xdrproc_t)xdr_void, (char*)argp,(xdrproc_t)xdr_dc_infox_back, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"dc_info clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -141,10 +159,13 @@ dc_devallx_back *dc_devall_1(void *argp,CLIENT *clnt,long *perr)
 {
 	static dc_devallx_back 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_DEVALL,(xdrproc_t)xdr_void, (char*)argp,(xdrproc_t)xdr_dc_devallx_back, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"dc_devall clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -163,10 +184,13 @@ dc_devinfx_back *dc_devinfo_1(name *argp,CLIENT *clnt,long *perr)
 {
 	static dc_devinfx_back 	res;
 	enum clnt_stat 		clnt_stat;
+	void (* old_sighandler)(int);
 
 	memset((char *)&res, 0, sizeof(res));
 
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, DC_DEVINFO,(xdrproc_t)xdr_name, (caddr_t)argp,(xdrproc_t)xdr_dc_devinfx_back, (caddr_t)&res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"dc_devinfo clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
@@ -186,8 +210,11 @@ dc_devinfx_back *dc_devinfo_1(name *argp,CLIENT *clnt,long *perr)
 int dc_rpcwr_check_clnt_1(CLIENT *clnt,char **res,long *perr)
 {
 	enum clnt_stat clnt_stat;
-
+	void (* old_sighandler)(int);	
+	
+	old_sighandler = signal(SIGPIPE,sigpipe_handler);
 	clnt_stat = clnt_call(clnt, RPC_CHECK,(xdrproc_t)xdr_void, NULL,(xdrproc_t)xdr_wrapstring,(caddr_t)res, TIMEOUT);
+	signal(SIGPIPE,old_sighandler);
 	if (clnt_stat != RPC_SUCCESS) 
 	{
 		dev_printdebug(DBG_ERROR,"rpc_check clnt_call returned : %s\n",clnt_sperrno(clnt_stat));
