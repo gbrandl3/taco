51f8d63f4e347493379a48723c6fbc6993d22046
jkrueger1 <jkrueger1@cd552e4a-acb0-437c-bff1-0b624b0be6ef>
2009-06-17 07:28:59 +0000

    add two lost prototypes again
    git-svn-id: https://svn.code.sf.net/p/taco/taco/trunk@1940 cd552e4a-acb0-437c-bff1-0b624b0be6ef

--- taco/lib/dbase/db_setup.h
+++ taco/lib/dbase/db_setup.h
@@ -25,9 +25,9 @@
  * Author(s):	
  *		$Author: jkrueger1 $
  * 
- * Version:	$Revision: 1.15 $
+ * Version:	$Revision: 1.16 $
  *
- * Date:	$Date: 2008-12-02 08:42:36 $
+ * Date:	$Date: 2009-06-17 07:28:59 $
  *
  ******************************************************************************/
 
@@ -486,6 +486,9 @@ long _DLLFunc db_upddev PT_((long devdef_nb,char **devdef,long *deferr_nb,DevLon
 long _DLLFunc db_updres PT_((long resdef_nb,char **resdef,long *deferr_nb,DevLong *error));
 long _DLLFunc db_secpass PT_((char **pass,DevLong *error));
 long _DLLFunc db_delete_update PT_((long dev_nb,char **dev_name_list,long list_type,long devdef_nb,char **devdef,long resdef_nb,char **resdef,db_delupd_error *p_error));
+
+int _DLLFunc db_getdevexp_tango PT_((char *filter,char ***device_tab,u_int *dev_num,long *error)); 	 
+int _DLLFunc db_freedevexp_tango PT_((char **buffer));
 
 #ifdef __cplusplus
 }
